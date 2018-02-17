6d52494cc3b91258e4f40b7e4dfdd6db31b1bf10
yihongXU <hongxu520@gmail.com>
2018-02-17 23:19:05 +0100

    add generate visualWords and create hists

--- test.py
+++ test.py
@@ -1,3 +1,48 @@
-from utils import generator_descriptor
+## this code is for searching similar images for a given image ##
 
-generator_descriptor('./train/', './descriptors_keypoints')
+from utils import searchFromBase, generateHist, getImageListFromDir
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+import cv2
+import numpy as np
+import os
+
+def main():
+    # definitions #
+
+    base_dir = "./train/" # base dir for search
+    model_dir = "./save_model/kmeans.pkl" # pretrained kmeans model
+    target_dir = "./test/514_c.png" # target to search
+    hist_addr = ''  # generated histograms for the dataset, if hist_addr = '', we will generate hists below
+    descriptor_tpye = 'orb'
+
+    # search similar images from base #
+    kmeans = joblib.load(model_dir) # load pretrained kmeans model
+    print ('kmeans parameters', kmeans.get_params())
+
+    # if hist_addr does not exist, generate hists for the dataset #
+    if hist_addr == '':
+        hist_addr = './hists/'
+        if os.path.exists(hist_addr) == False:
+            os.mkdir(hist_addr[0:-1])
+        imgs_addr = getImageListFromDir(base_dir)
+        for addr in imgs_addr :
+            print addr
+            img = cv2.imread(addr)
+            img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            hist = generateHist(kmeans, img_gs, 'image', descriptor_tpye)
+            filename = addr.split('/')[-1][0:-4]
+            print filename
+            np.save(hist_addr+filename+'_'+descriptor_tpye, hist)
+
+    target = cv2.imread(target_dir)
+    results, imgs_list = searchFromBase(hist_addr, target,kmeans, has_hist=True)
+    count = 1
+    for key, value in results:
+        filename = imgs_list[key].split('/')[-1]
+        print ('NO. ' + str(count) +' is: ' + filename)
+        print ("with distance from the target : " + str(value))
+        count += 1
+
+if __name__ == '__main__':
+    main()
---
+++ visualWordsGen.py
@@ -0,0 +1,64 @@
+### this python module aims for creating visualWords from clustering the base. ###
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import numpy as np
+import glob
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+from utils import generator_descriptor
+import os
+def main():
+    # definitions #
+    train_addr = './train/' # path where train images lie
+    descpts_addr = ''  # path where are saved the descriptors, If descripts_addr = '', create them below
+    desptype='orb'  # type of descriptors to be generated
+
+
+    # read or generate local descriptors from the base (saved as numpy array). #
+
+    # if descriptors not exist, create them here !
+    if descpts_addr == '':
+        descpts_addr = "./dscpt_32bits_orb"
+        if os.path.exists(descpts_addr) == False:
+            os.mkdir(descpts_addr)
+        generator_descriptor(train_addr, descpts_addr, desp_type=desptype)
+        descpts_addr = descpts_addr+"/*.npy" # path where are saved the descriptors
+
+    # each npy file has all local descriptors for an image
+    descpts_list = glob.glob(descpts_addr)
+    print descpts_list
+
+    # generate train data for k means clustering of shape [num of descriptors, dimension of descriptor]
+    train_data = None
+    files_no_despt = []
+    for strr in descpts_list:
+        ret = strr.find('kp')
+        if strr.find('kp') == -1: # not keypoints array
+
+            if train_data is None:  # first arr
+                tmp = np.load(strr)
+                train_data = tmp
+
+            elif np.load(strr).shape != () and np.load(str).shape[1] == train_data.shape[1]:
+                print strr
+                tmp = np.load(strr)
+                train_data = np.vstack((train_data, np.load(strr)))
+
+            else:
+                files_no_despt.append(strr)  # images that we cannot extract keypoints and descriptors
+
+    print train_data.shape
+    print files_no_despt
+
+    # k-means clustering for train_data
+    kmeans = KMeans(n_clusters=400, random_state=0).fit(train_data)
+    print kmeans.labels_
+    print kmeans.cluster_centers_
+    if os.path.exists('./save_model') == False:
+        os.mkdir('./save_model')
+    joblib.dump(kmeans, './save_model/kmeans_40.pkl')
+    joblib.dump(kmeans, './save_model/kmeans_'+str(kmeans.get_params()['n_clusters'])+'.pkl')
+
+main()
+
+
